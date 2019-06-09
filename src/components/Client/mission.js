195f9f3299cbf253b31472cad1fa9ed20fbc01e5
Wren <drawnwren@gmail.com>
2019-06-09 18:18:54 +0800

    Mission css, readme, and added the mission to gatsby-config.js

--- src/components/Client/mission.js
+++ src/components/Client/mission.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-const IndexMission = props => (
-  <div className="index-mission-container">
-    <p className="index-mission">
-      Our mission is to ullamcorper, vel facilisis diam rhoncus. Nunc porttitor
-      at est sodales ultricies. Nullam ut odio nec libero vestibulum luctus sit
-      amet in velit. Donec commodo semper urna. Aenean mollis est et fringilla
-      sodales. Vestibulum sed pharetra tortor, a molestie urna. Interdum et
-      malesuada fames ac ante ipsum primis in faucibus. Etiam eu leo efficitur,
-      feugiat metus in, fringilla mauris. Sed lectus metus, malesuada sit amet
-      dui id, accumsan ultrices justo. Sed dapibus aliquet tempor. Curabitur
-      libero leo, eleifend a massa et, finibus lobortis nisl. Aliquam sed
-      volutpat nibh. Phasellus tempor ut ex a auctor.
+import style from '../../styles/Mission.module.sass'
+
+const IndexMission = ({ mission }) => (
+  <div className={style.indexMissionContainer}>
+    <p className={style.indexMission}>
+      <span className={style.firstLetter}>{mission[0]}</span>
+      {mission.slice(1)}{' '}
     </p>
   </div>
 )
--- src/components/client.js
+++ src/components/client.js
@@ -1,16 +1,31 @@
 import React from 'react'
+import { StaticQuery } from 'gatsby'
+
 import IndexMission from './Client/mission.js'
 import IndexPoints from './Client/IndexPoints.js'
 import style from '../styles/Client.module.sass'
 
 const Client = () => (
-  <div className={style.clientIndexPage}>
-    <div>
-      <div className={style.indexSplashContainer} />
-      <IndexPoints />
-      <IndexMission />
-    </div>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query SiteMission {
+        site {
+          siteMetadata {
+            mission
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className={style.clientIndexPage}>
+        <div>
+          <div className={style.indexSplashContainer} />
+          <IndexPoints />
+          <IndexMission mission={data.site.siteMetadata.mission} />
+        </div>
+      </div>
+    )}
+  />
 )
 
 export default Client
