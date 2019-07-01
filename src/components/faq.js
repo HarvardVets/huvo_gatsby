f0a7b592139faaba5537d6286e8b5bb0640cccda
Wren <drawnwren@gmail.com>
2019-07-01 16:40:50 +0800

    Fixed navbar collapse element, working on FAQ style

--- src/components/faq.js
+++ src/components/faq.js
@@ -1,46 +1,43 @@
 import React from 'react'
 import style from './../styles/faqs.module.sass'
 
-/*
-  this.props.faqs = {
-  category: [{
-  {
-  "question": "Can I get into Harvard?",
-  "answer": "No",
-  "category": "admissions"
-  }
-
-  }]
-  }
-   */
-
 export default class FAQ extends React.Component {
-  render() {
-    let questions = this.props.faqs
+  constructor(props) {
+    super(props)
+    let questions = props.faqs
     let categories = []
     console.log(questions)
     Object.keys(questions).forEach(category => {
       let category_div = (
         <div className={style.category} key={category}>
-          <h3>{category}</h3>
+          <div>
+            <span>{category}</span>
+            <span />
+          </div>
           <div className={style.faqs}>
             {questions[category].map((question, i) => (
               <div className={style.faq} key={i}>
                 <div className={style.question} key={style.question}>
-                  <p className={style.Q}>Q:</p>
-                  <p> {question.question}</p>
+                  <span className={style.Q}>Q:</span>
+                  <span> {question.question}</span>
                 </div>
                 <div className={style.answer} key={style.answer}>
-                  <p className={style.A}>A:</p>
-                  <p> {question.answer}</p>
+                  <span className={style.A}>A:</span>
+                  <span> {question.answer}</span>
                 </div>
               </div>
             ))}
+            <br />
           </div>
         </div>
       )
       categories.push(category_div)
     })
-    return categories
+    this.state = {
+      categories,
+    }
+  }
+  render() {
+    return <div className={style.wrapper}> {this.state.categories} </div>
   }
 }
