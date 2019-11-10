25a52528806b3b17d6d54fe0cbd1317d5ae2ce7f
Wren <9318247+DrawnWren@users.noreply.github.com>
2019-11-10 15:31:08 -0500

    Working on patrons page

--- src/components/patrons.js
+++ src/components/patrons.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-import style from "./../styles/client.module.sass"
+import style from "./../styles/patrons.module.sass"
 
-const Client = () => <h1>Coming soon</h1>
+const Patrons = () => (
+  <div className={style.patronsSplashContainer}>
+    <h1>Coming soon</h1>
+  </div>
+)
 
 export default Patrons
---
+++ src/pages/patrons.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Patrons from "../components/patrons.js"
+import Layout from "../components/layout.js"
+
+const PatronsPage = () => (
+  <Layout>
+    <Patrons />
+  </Layout>
+)
+
+export default PatronsPage
